0045cf89177c8e8ed7ce73ebb48d761dddd700c4
tegar-ganang <tegarsatrio10@gmail.com>
2022-11-30 15:56:36 +0700

    edit

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,6 @@
 $host = "localhost"; // Nama hostnya
 $username = "root"; // Username
 $password = ""; // Password 
-$database = "mynotescode"; // Nama databasenya
+$database = "myno"; // Nama databasenya
 // Koneksi ke MySQL dengan PDO
 $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $database, $username, $password);
