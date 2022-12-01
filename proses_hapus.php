50272efbc6c353cdc3ee205e50638da351873666
tegar-ganang <tegarsatrio10@gmail.com>
2022-12-01 14:04:56 +0700

    edit

--- proses_hapus.php
+++ proses_hapus.php
@@ -4,21 +4,22 @@ include "koneksi.php";
 // Ambil data NIS yang dikirim oleh index.php melalui URL
 $id = $_GET['id'];
 // Query untuk menampilkan data siswa berdasarkan ID yang dikirim
-$sql = $pdo->prepare("SELECT foto FROM siswa WHERE id=:id");
+$sql = $pdo->prepare("SELECT foto FROM idsiswa WHERE id=:id");
 $sql->bindParam(':id', $id);
 $sql->execute(); // Eksekusi query insert
 $data = $sql->fetch(); // Ambil semua data dari hasil eksekusi $sql
 // Cek apakah file fotonya ada di folder images
-if (is_file("images/" . $data['foto'])) // Jika foto ada
-    unlink("images/" . $data['foto']); // Hapus foto yang telah diupload dari folder images
+if(is_file("images/".$data['foto'])) // Jika foto ada
+  unlink("images/".$data['foto']); // Hapus foto yang telah diupload dari folder images
 // Query untuk menghapus data siswa berdasarkan ID yang dikirim
-$sql = $pdo->prepare("DELETE FROM siswa WHERE id=:id");
+$sql = $pdo->prepare("DELETE FROM idsiswa WHERE id=:id");
 $sql->bindParam(':id', $id);
 $execute = $sql->execute(); // Eksekusi / Jalankan query
-if ($execute) { // Cek jika proses simpan ke database sukses atau tidak
-    // Jika Sukses, Lakukan :
-    header("location: index.php"); // Redirect ke halaman index.php
-} else {
-    // Jika Gagal, Lakukan :
-    echo "Data gagal dihapus. <a href='index.php'>Kembali</a>";
+if($execute){ // Cek jika proses simpan ke database sukses atau tidak
+  // Jika Sukses, Lakukan :
+  header("location: index.php"); // Redirect ke halaman index.php
+}else{
+  // Jika Gagal, Lakukan :
+  echo "Data gagal dihapus. <a href='index.php'>Kembali</a>";
 }
+?>
--- proses_simpan.php
+++ proses_simpan.php
@@ -10,30 +10,31 @@ $alamat = $_POST['alamat'];
 $foto = $_FILES['foto']['name'];
 $tmp = $_FILES['foto']['tmp_name'];
 // Rename nama fotonya dengan menambahkan tanggal dan jam upload
-$fotobaru = date('dmYHis') . $foto;
+$fotobaru = date('dmYHis').$foto;
 // Set path folder tempat menyimpan fotonya
-$path = "images/" . $fotobaru;
+$path = "images/".$fotobaru;
 // Proses upload
-if (move_uploaded_file($tmp, $path)) { // Cek apakah gambar berhasil diupload atau tidak
-    // Proses simpan ke Database
-    $sql = $pdo->prepare("INSERT INTO siswa(nis, nama, jenis_kelamin, telp, alamat, foto) VALUES(:nis,:nama,:jk,:telp,:alamat,:foto)");
-    $sql->bindParam(':nis', $nis);
-    $sql->bindParam(':nama', $nama);
-    $sql->bindParam(':jk', $jenis_kelamin);
-    $sql->bindParam(':telp', $telp);
-    $sql->bindParam(':alamat', $alamat);
-    $sql->bindParam(':foto', $fotobaru);
-    $sql->execute(); // Eksekusi query insert
-    if ($sql) { // Cek jika proses simpan ke database sukses atau tidak
-        // Jika Sukses, Lakukan :
-        header("location: index.php"); // Redirect ke halaman index.php
-    } else {
-        // Jika Gagal, Lakukan :
-        echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-        echo "<br><a href='form_simpan.php'>Kembali Ke Form</a>";
-    }
-} else {
-    // Jika gambar gagal diupload, Lakukan :
-    echo "Maaf, Gambar gagal untuk diupload.";
+if(move_uploaded_file($tmp, $path)){ // Cek apakah gambar berhasil diupload atau tidak
+  // Proses simpan ke Database
+  $sql = $pdo->prepare("INSERT INTO idsiswa(nis, nama, jenis_kelamin, telp, alamat, foto) VALUES(:nis,:nama,:jk,:telp,:alamat,:foto)");
+  $sql->bindParam(':nis', $nis);
+  $sql->bindParam(':nama', $nama);
+  $sql->bindParam(':jk', $jenis_kelamin);
+  $sql->bindParam(':telp', $telp);
+  $sql->bindParam(':alamat', $alamat);
+  $sql->bindParam(':foto', $fotobaru);
+  $sql->execute(); // Eksekusi query insert
+  if($sql){ // Cek jika proses simpan ke database sukses atau tidak
+    // Jika Sukses, Lakukan :
+    header("location: index.php"); // Redirect ke halaman index.php
+  }else{
+    // Jika Gagal, Lakukan :
+    echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
     echo "<br><a href='form_simpan.php'>Kembali Ke Form</a>";
+  }
+}else{
+  // Jika gambar gagal diupload, Lakukan :
+  echo "Maaf, Gambar gagal untuk diupload.";
+  echo "<br><a href='form_simpan.php'>Kembali Ke Form</a>";
 }
+?>
